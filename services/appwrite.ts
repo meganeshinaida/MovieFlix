00b5287a2eb755fdfe6780e9d933abce0856dccf
meganeshinaida <156678792+meganeshinaida@users.noreply.github.com>
2025-12-09 09:07:50 +0100

    .env removal

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -2,7 +2,11 @@
 const DATABSE_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
-
+const client = new Client()
+.setEndpoint('http://cloud.appwrite.io/v1')
+.setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    // const database = new DatabaseSync(client)
+)
 export const updateSearchCount = async (query:string,movie:Movie)=>{
     // check if a record of that search has already been stored
     // if a document is found increment the searchCount field
